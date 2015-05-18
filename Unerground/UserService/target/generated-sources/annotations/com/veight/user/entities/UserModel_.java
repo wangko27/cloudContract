5dcf496b8fc9f6c9a1f5b60e4a6098601e942482
wangko27 <845885222@qq.com>
2015-05-18 20:56:21 +0800

    开始更换为2.0版本

--- Unerground/UserService/target/generated-sources/annotations/com/veight/user/entities/UserModel_.java
+++ Unerground/UserService/target/generated-sources/annotations/com/veight/user/entities/UserModel_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-05-17T18:36:58")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-05-18T20:52:35")
 @StaticMetamodel(UserModel.class)
 public class UserModel_ { 
 
--- jersey-sample/src/main/java/br/net/woodstock/pki/web/security/SimpleUser.java
+++
@@ -1,9 +0,0 @@
-package br.net.woodstock.pki.web.security;
-
-import java.io.Serializable;
-
-public interface SimpleUser extends Serializable {
-
-    String[] getRoles();
-
-}
